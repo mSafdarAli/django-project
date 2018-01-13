6262345f83115848d87af0b6934f195cb3f31357
M Safdar Ali Khan <msafdaralikhan65@gmail.com>
2018-01-13 12:04:54 +0500

    About id and displaying saved data

--- myproject/restaurants/views.py
+++ myproject/restaurants/views.py
@@ -1,12 +1,35 @@
+from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import RestaurantLocation
 
 # Create your views here.
 def restaurant_listview(request):
 	template_name = "restaurants/restaurants_list.html"
+	queryset = RestaurantLocation.objects.all()
 	context = {
-		"object_list": [12, 14, 445, 45]
+	"object_list": queryset
 	}
 	return render(request, template_name, context)
+
+class RestaurantListView(ListView):		
+	def get_queryset(self):
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(
+			Q(category__iexact = slug) |
+			Q(category__icontains = slug)
+			)
+		else:
+			queryset = RestaurantLocation.objects.all()
+		return queryset
+
+class RestaurantDetailView(DetailView):		
+	queryset = RestaurantLocation.objects.all()
+
+	def get_object(self, *args, **kwargs):
+		rest_id = self.kwargs.get('rest_id')
+		obj = get_object_or_404(RestaurantLocation, id=rest_id) #pk = rest_id
+		return obj
